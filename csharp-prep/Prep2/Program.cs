1300d36d4635739f0d64af20e9706a39d408ee77
jooshhart <jooshhart@gmail.com>
2024-01-09 13:34:07 -0700

    y

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? (No decimal)");
+        string val = Console.ReadLine();
+        int per = int.Parse(val);
+        string letter = "";
+
+        if (per >= 90)
+        {
+            letter = "Your grade is an A";
+        }
+        else if (per >= 80)
+        {
+            letter = "Your grade is a B";
+        }
+        else if (per >= 70)
+        {
+            letter = "Your grade is a C";
+        }
+        else if (per >= 60)
+        {
+            letter = "Your grade is a D";
+        }
+        else {letter = "Your grade is an F";};
+        Console.WriteLine(letter);
+        if (per >= 70) {Console.WriteLine("You have passed the class!");};
     }
 }
