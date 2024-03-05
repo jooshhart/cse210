d68b96c056b53ebf2287c678f19e832502787812
jooshhart <jooshhart@gmail.com>
2024-03-05 10:04:43 -0700

    y

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square a = new Square("Orange", 5);
+        shapes.Add(a);
+        Rectangle b = new Rectangle("Yellow", 5, 5);
+        shapes.Add(b);
+        Circle c = new Circle("Red", 5);
+        shapes.Add(c);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
